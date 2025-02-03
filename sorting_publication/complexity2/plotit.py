50c66147ca1c73604cb79aad23dc65bbc3985482
Peter Taraba <151561981+peta78@users.noreply.github.com>
2025-02-03 10:35:55 -0800

    Add files via upload
    this confirms O(n log n) for std sort and O(n log log n) for stat sort.

--- sorting_publication/complexity2/plotit.py
+++ sorting_publication/complexity2/plotit.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import math
 import matplotlib.pyplot as plt
+import sys
 
-fn = 'log_uniform.csv'
+fn = 'log_' + sys.argv[1] + '.csv'
 
 f = open(fn, 'rt')
 lines = f.readlines()
@@ -23,34 +24,41 @@ for line in lines:
     y1.append(float((s2-s1).total_seconds()))
     y2.append(float((s3-s2).total_seconds()))
 
-difs1 = []
-difs2 = []
-difs3 = []
-win1 = 0
-win2 = 0
-win3 = 0
-for i in range(1,len(x)):
-    for j in range(i+1,len(x)):
-        real = y2[j]/y2[i]
-        on = (x[j])/(x[i])
-        onlogn = (x[j]*math.log(x[j]))/(x[i]*math.log(x[i]))
-        onloglogn = (x[j]*math.log(math.log(x[j])))/(x[i]*math.log(math.log(x[i])))
+def plotdist(x,y, which):
+    difs1 = []
+    difs2 = []
+    difs3 = []
+    win1 = 0
+    win2 = 0
+    win3 = 0
+    for i in range(1,len(x)):
+        for j in range(i+1,len(x)):
+            real = y[j]/y[i]
+            on = (x[j])/(x[i])
+            onlogn = (x[j]*math.log(x[j]))/(x[i]*math.log(x[i]))
+            onloglogn = (x[j]*math.log(math.log(x[j])))/(x[i]*math.log(math.log(x[i])))
 
-        difs1.append(abs(real-on))
-        difs2.append(abs(real-onlogn))
-        difs3.append(abs(real-onloglogn))
+            difs1.append(abs(real-on))
+            difs2.append(abs(real-onlogn))
+            difs3.append(abs(real-onloglogn))
 
-        if abs(real-on)<abs(real-onlogn) and abs(real-on)<abs(real-onloglogn):
-            win1 += 1
-        elif abs(real-onlogn)<abs(real-onloglogn):
-            win2 += 1
-        else:
-            win3 += 1
+            if abs(real-on)<abs(real-onlogn) and abs(real-on)<abs(real-onloglogn):
+                win1 += 1
+            elif abs(real-onlogn)<abs(real-onloglogn):
+                win2 += 1
+            else:
+                win3 += 1
 
-print(win1,win2,win3)
-print(sum(difs1)/len(difs1),sum(difs2)/len(difs2),sum(difs3)/len(difs3))
-plt.hist(difs1,bins=30, color='r', histtype='step', density=True, label='O(n)')
-plt.hist(difs2,bins=30, color='g', histtype='step', density=True, label='O(n log n)')
-plt.hist(difs3,bins=30, color='b', histtype='step', density=True, label='O(n log log n)')
-plt.legend()
-plt.show()
+    print('O(n)', 'O(n log n)', 'O(n log log n)')
+    print(win1,win2,win3)
+    print(sum(difs1)/len(difs1),sum(difs2)/len(difs2),sum(difs3)/len(difs3))
+    plt.figure(figsize=(15, 10))
+    plt.hist(difs1,bins=100, color='r', histtype='step', density=True, label='O(n)')
+    plt.hist(difs2,bins=100, color='g', histtype='step', density=True, label='O(n log n)')
+    plt.hist(difs3,bins=100, color='b', histtype='step', density=True, label='O(n log log n)')
+    plt.legend()
+    plt.xlim(0.0, 2.0)
+    plt.savefig('fig_dist_anal_' + sys.argv[1] + '_' + which + '.png')
+
+plotdist(x,y1, 'stdsort')
+plotdist(x,y2, 'statsort')
